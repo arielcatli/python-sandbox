2936186f98c2a21f509cd721ef2d894dea2937f0
Ariel Catli <arielcatli@gmail.com>
2018-02-03 22:26:29 +0800

    added Python notes
    Added the Python notes from Murach's Python book

--- gui.py
+++ gui.py
@@ -10,13 +10,28 @@ Created on Sat Feb  3 21:23:01 2018
 import tkinter as tk
 from tkinter import ttk
 
+def btn1_click():
+    root.title("Changing the title")
+
+def btn2_click():
+    root.destroy()
+    
 root = tk.Tk()
 root.title("My first GUI program in Python")
 root.geometry("300x200")
 frame = ttk.Frame(root, padding="10 50 30 40")
 frame.pack()
-button1 = ttk.Button(frame, text="Ok")
+ttk.Label(frame, text="This is the first button").pack()
+button1 = ttk.Button(frame, text="Ok", command=btn1_click)
 button1.pack(fill=tk.BOTH, expand = True)
-button2 = ttk.Button(frame, text="Cancel")
+ttk.Label(frame, text="This is the last button").pack()
+button2 = ttk.Button(frame, text="Cancel", comman=btn2_click)
 button2.pack()
+sometext = tk.StringVar()
+tbox1 = ttk.Entry(frame, width=10, textvariable=sometext)
+sometext.set("Type something")
+tbox1.pack()
 root.mainloop()
+
+
+
